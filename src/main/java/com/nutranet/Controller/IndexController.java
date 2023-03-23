df39f3499fe9750c50a85134feab680cae38fc33
HDJ <johorea1356@gmail.com>
2023-03-23 23:02:29 +0900

    day03. ResponseEntity

--- src/main/java/com/nutranet/Controller/IndexController.java
+++ src/main/java/com/nutranet/Controller/IndexController.java
@@ -1,18 +1,23 @@
 package com.nutranet.Controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
 
-    @GetMapping ({"","/"})
-    public String main(){
-        return "/api/hello";
-    }
+
 
     @GetMapping ("/api/hello")
     public String test(){
         return "hi, hello!!";
+    }
+
+    @GetMapping ({"","/"})
+    public ResponseEntity<?> Login(){
+        return new ResponseEntity<String>("ok", HttpStatus.OK);
     }
 }
