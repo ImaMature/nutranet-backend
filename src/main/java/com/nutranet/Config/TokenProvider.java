3304cf4a3c10be6e4231e25936bda8999712d081
HDJ <johorea1356@gmail.com>
2023-03-28 00:27:15 +0900

    로그인을 위해 JWT 토큰 도입 중(Key 문제 해결)

--- src/main/java/com/nutranet/Config/TokenProvider.java
+++ src/main/java/com/nutranet/Config/TokenProvider.java
@@ -4,8 +4,10 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.security.Key;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
@@ -16,25 +18,40 @@ import java.util.Date;
 public class TokenProvider {
 
     // JWT 생성 및 검증을 위한 키
-    private static final Key SECURITY_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS512); //JWT만들 때 사용할 키
+    //application.properties로부터 가져옴
+    @Value("${jwt.token.key}")
+    private String SECURITY_KEY;
     // 위 키를 바탕으로 데이터 암호화, 복호화
+
+    // 객체 초기화, secretKey를 Base64로 인코딩한다.
+    @PostConstruct
+    protected void init() {
+        SECURITY_KEY = Base64.getEncoder().encodeToString(SECURITY_KEY.getBytes());
+    }
 
     // JWT 생성하는 메소드
     public String create (String mid){
         // 만료 시간                                             // 현재시간 + 1일
 
-        String encodedKey = Base64.getEncoder().encodeToString(SECURITY_KEY.getEncoded());
-        System.out.println(encodedKey);
-
-        Date exprTime = (Date) Date.from(Instant.now().plus(1, ChronoUnit.DAYS));
+        //비밀 키로 만들기
+        //String -> byte[]
+        byte[] decodedKeyBytes = Base64.getDecoder().decode(SECURITY_KEY);
+        //byte[] -> Key
+        Key privateKey = Keys.hmacShaKeyFor(decodedKeyBytes);
         try {
-            System.out.println("create 넘어옴? key는 ? ==> "+ encodedKey);
+            String encodedKey = Base64.getEncoder().encodeToString(privateKey.getEncoded());
+            System.out.println("@@비밀 키 : "+encodedKey);
+            System.out.println("@@원본 String 키 : "+ SECURITY_KEY);
+
+            Date exprTime = (Date) Date.from(Instant.now().plus(1, ChronoUnit.DAYS));
+
+            //System.out.println("create 넘어옴? key는 ? ==> "+ encodedKey);
             // JWT 생성
             return Jwts.builder()
                     // 암호화에 사용될 알고리즘, 키
-                    //.signWith(알고리즘(enum), 키(String)) -> 옛날 버전
-                    //.signWith(키(Key), 알고리즘(enum)) -> 현재 버전
-                    .signWith(SECURITY_KEY, SignatureAlgorithm.HS512)
+                        //.signWith(알고리즘(enum), 키(String)) -> 옛날 버전
+                        //.signWith(키(Key), 알고리즘(enum)) -> 현재 버전
+                    .signWith(privateKey, SignatureAlgorithm.HS512)
                     // Subject(JWT 제목), Issue(생성일), Expiration(만료일) 설정
                     .setSubject(mid) //제목
                     .setIssuedAt(new Date()) //현재 시간 기반으로 생성
